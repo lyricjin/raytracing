e05262e589cd2f16cdd9888696909289782dca6b
jin <lyricjin@foxmail.com>
2017-04-20 22:07:47 +0800

    加了个Ray的class

---
+++ project/RayTracing/RayTracing/Ray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracing
+{
+    class Ray
+    {
+        public Vector3 Origin { get; }
+        public Vector3 Direction { get; }
+
+        public Ray(Vector3 origin, Vector3 direction)
+        {
+            Origin = origin;
+            Direction = direction;
+            Direction.Normalize();
+        }
+
+        public Vector3 PointAtDistance(float distance)
+        {
+            Vector3 point = Origin + distance * Direction;
+            return point;
+        }
+    }
+}
