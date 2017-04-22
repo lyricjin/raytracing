d2ebc49adb59997627c28c4de9a13c931aa220a6
jin <lyricjin@foxmail.com>
2017-04-22 11:44:43 +0800

    像素转换成射线，射线转换成颜色。 觉得Vector里面加的两个方法名称以及方法所归属的类都还不太合理，需要改造。

--- project/RayTracing/RayTracing/Program.cs
+++ project/RayTracing/RayTracing/Program.cs
@@ -8,14 +8,22 @@ namespace RayTracing
 {
     class Program
     {
+        static Vector3 GetColor(Ray ray)
+        {
+            Vector3 color = 255 * ray.Direction.Abs();
+            return color.IntValue();
+        }
+
         static void Main(string[] args)
         {
+            Camera camera = new Camera(4f, 2f, 2f);
             PPMMap map = new PPMMap(400, 200);
             for (int row = 0; row < map.Height; row++)
             {
                 for (int col = 0; col < map.Width; col++)
                 {
-                    map.SetColor(col, row, new Vector3(255 * col / map.Width, 255 * row / map.Height, 0));
+                    Ray ray = camera.GetRay((double)col / map.Width, (double)row / map.Height);
+                    map.SetColor(col, row, GetColor(ray));
                 }
             }
             map.Save("raytracing.ppm");
