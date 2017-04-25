2ab587995abf37d9cc4c7d14bfa9d75315d0efd0
jin <lyricjin@foxmail.com>
2017-04-25 14:28:55 +0800

    加了个Sphere的类出来，它会负责计算一个射线，是否射中了它。

--- project/RayTracing/RayTracing/Program.cs
+++ project/RayTracing/RayTracing/Program.cs
@@ -2,9 +2,40 @@
 
 namespace RayTracing
 {
+    class HitResult
+    {
+        public bool Available;
+        public Vector3 HitPoint;
+        public Vector3 Normal;
+
+        public HitResult(bool available, Vector3 hitPoint, Vector3 normal)
+        {
+            Available = available;
+            HitPoint = hitPoint;
+            Normal = normal;
+        }
+    }
+
+    class Sphere
+    {
+        private Vector3 _center;
+        private double _radius;
+
+        public Sphere(Vector3 center, double radius)
+        {
+            _center = center;
+            _radius = radius;
+        }
+
+        public HitResult Hit(Ray ray)
+        {
+            return new HitResult(true, _center, _center * _radius);
+        }
+    }
+
     class Program
     {
-        static Vector3 GetColor(Ray ray)
+        static Vector3 GetColor(Sphere sphere, Ray ray)
         {
             double t = 0.5 * (ray.Direction.Y + 1.0);
             Vector3 color = (1.0 - t) * (new Vector3(1.0 * 255, 1.0 * 255, 1.0 * 255)) +
@@ -16,13 +47,14 @@ namespace RayTracing
         static void Main(string[] args)
         {
             Camera camera = new Camera(4f, 2f, 2f);
+            Sphere sphere = new Sphere(new Vector3(0, 0, -1.0), 0.5);
             PPMMap map = new PPMMap(400, 200);
             for (int row = 0; row < map.Height; row++)
             {
                 for (int col = 0; col < map.Width; col++)
                 {
                     Ray ray = camera.GetRay((double)col / map.Width, (double)row / map.Height);
-                    map.SetColor(col, row, GetColor(ray));
+                    map.SetColor(col, row, GetColor(sphere, ray));
                 }
             }
             map.Save("raytracing.ppm");
